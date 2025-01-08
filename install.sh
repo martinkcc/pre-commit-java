20a1b8919c4632db53bb5cc7d404de5dc9663689
martinkcc <martin.joseph@kubota.com>
2025-01-08 13:30:18 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PMD_RELEASE="https://github.com/pmd/pmd/releases/download/pmd_releases/7.9.0/pmd-dist-7.9.0-bin.zip"
 echo "$PMD_RELEASE"
-PMD=$(echo "$PMD_RELEASE" | tr -d '\r')
+PMD=$(echo "$PMD_RELEASE" | tr -d '%0D')
 echo "$PMD"
 apt-get update && apt-get install --no-install-recommends -y ca-certificates wget unzip curl bash jq
 cd /opt | tr -d '/r'
