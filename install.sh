dc81eee550eaec9fb13ab87aee319f9eeba55a23
martinkcc <martin.joseph@kubota.com>
2025-01-08 13:13:33 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 PMD_RELEASE="https://github.com/pmd/pmd/releases/download/pmd_releases/7.9.0/pmd-dist-7.9.0-bin.zip"
-PMD_RELEASE=$(echo $PMD_RELEASE|tr -d '\n')
+PMD_RELEASE=$(echo $PMD_RELEASE|tr -d '\r')
 CHECKSTYLE_RELEASE="https://github.com/checkstyle/checkstyle/releases/download/checkstyle-10.21.1/checkstyle-10.21.1-all.jar"
-CHECKSTYLE_RELEASE=$(echo $CHECKSTYLE_RELEASE|tr -d '\n')
+CHECKSTYLE_RELEASE=$(echo $CHECKSTYLE_RELEASE|tr -d '\r')
 apt-get update && apt-get install --no-install-recommends -y ca-certificates wget unzip curl bash jq
 cd /opt | tr -d '\r
 wget -nc -O "$PMD_RELEASE" | tr -d '\r > pmd.zip && unzip pmd.zip && rm pmd.zip && mv pmd-bin* pmd && chmod -R +x pmd
