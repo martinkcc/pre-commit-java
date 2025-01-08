2d0a592e70a0d6fa6d5c3ddceb5bcf0d8b7a5006
martinkcc <martin.joseph@kubota.com>
2025-01-08 12:52:07 -0600

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-set -e
 PMD_RELEASE="https://github.com/pmd/pmd/releases/download/pmd_releases/7.9.0/pmd-dist-7.9.0-bin.zip"
-PMD_RELEASE=$(echo "$PMD_RELEASE" | sed 's/%0D//g')
 CHECKSTYLE_RELEASE="https://github.com/checkstyle/checkstyle/releases/download/checkstyle-10.21.1/checkstyle-10.21.1-all.jar"
-CHECKSTYLE_RELEASE=$(echo "$CHECKSTYLE_RELEASE" | sed 's/%0D//g')
 apt-get update && apt-get install --no-install-recommends -y ca-certificates wget unzip curl bash jq
 cd /opt
 wget -nc -O pmd.zip "$PMD_RELEASE" && unzip pmd.zip && rm pmd.zip && mv pmd-bin* pmd && chmod -R +x pmd
